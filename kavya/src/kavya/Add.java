0cc956ae7c157cf5890fd108bb147ca557de360a
MSrikavya <machineni.srikavya4@gmail.com>
2023-01-11 01:33:17 -0500

    Check

---
+++ kavya/src/kavya/Add.java
@@ -0,0 +1,15 @@
+package kavya;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add a = new Add();
+		a.add(1, 2);
+	}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
