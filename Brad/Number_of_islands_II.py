6d38db13a8a27997be2064bdbc42ed47ebcdc3d1
Bhardwaz Reddy <66362067+bhardwaz182@users.noreply.github.com>
2023-01-14 00:31:36 -0500

    Update Number_of_islands_II.py

--- Brad/Number_of_islands_II.py
+++ Brad/Number_of_islands_II.py
@@ -1,30 +1,23 @@
-def numIslands2(m,n,positions):
-    def visitedIsland(i,j,m,n):
-        global grid
-        grid[i][j]=0
-        if i-1>=0 and grid[i-1][j]:
-            visitedIsland(i-1,j,m,n)
-        if i+1<=m-1 and grid[i+1][j]:
-            visitedIsland(i+1,j,m,n)
-        if j-1>=0 and grid[i][j-1]:
-            visitedIsland(i,j-1,m,n)
-        if j+1<=n-1 and grid[i][j+1]:
-            visitedIsland(i,j+1,m,n)
-    
-    def numIslands(grid,m,n):
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]:
-                    count+=1
-                    visitedIsland(i,j,m,n)
-        return count
-    grid1 = [[0 for _ in range(m)] for i in range(n)]
+def numIslands3(m,n,positions):
+    def checkForIslands(i,j,m,n):
+        if i-1>=0 and grid1[i-1][j]:
+            return False
+        if i+1<=m-1 and grid1[i+1][j]:
+            return False
+        if j-1>=0 and grid1[i][j-1]:
+            return False
+        if j+1<=n-1 and grid1[i][j+1]:
+            return False
+        return True
+    global grid1
+    grid1 = [[0 for _ in range(n)] for i in range(m)]
     l=[]
-    for val in positions:
-        grid1[val[0]][val[1]] = 1
-        global grid
-        grid = [array[:] for array in grid1]
-        l.append(numIslands(grid,m,n))
+    count=0
+    for [i,j] in positions:
+        grid1[i][j] = 1
+        if checkForIslands(i,j,m,n):
+            count+=1
+            
+        l.append(count)
         
     return l
