6b57cc0f9db1ba94d39eaf38829450027edc38e7
MSrikavya <machineni.srikavya4@gmail.com>
2023-01-13 22:06:51 -0500

    Added program

---
+++ Kavya/Add.java
@@ -0,0 +1,13 @@
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add obj = new Add();
+		System.out.println(obj.add(1, 2));
+	}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
